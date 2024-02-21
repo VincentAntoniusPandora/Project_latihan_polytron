c7701ee6de9913ae3eb991ea1929c3b7c8583820
Vincent <vincentantoniusp@gmail.com>
2024-02-21 14:11:18 +0700

    test

--- app/Views/add_new_employee.php
+++ app/Views/add_new_employee.php
@@ -9,17 +9,16 @@
             <!-- employee name -->
             <h1>Add New Employee</h1>
             <?php if (session()->has('errors')) : ?>
-            <div class="alert alert-danger" role="alert">
-                <?php foreach (session('errors') as $error) : ?>
-                <strong><?= esc($error) ?></strong>
-                <br>
-                <?php endforeach; ?>
-            </div>
+                <div class="alert alert-danger" role="alert">
+                    <?php foreach (session('errors') as $error) : ?>
+                        <strong><?= esc($error) ?></strong>
+                        <br>
+                    <?php endforeach; ?>
+                </div>
             <?php endif; ?>
             <div class="form-group ">
                 <label for="formGroupExampleInput">Employee Name</label>
-                <input type="text" name="employee_name" class="form-control" id="formGroupExampleInput"
-                    placeholder="Enter new employee name*">
+                <input type="text" name="employee_name" class="form-control" id="formGroupExampleInput" placeholder="Enter new employee name*">
             </div>
             <br>
 
@@ -45,12 +44,12 @@
                 <p style="font-size: 13px; color:#990011;">*Please Input Your Birthday Before Today's Date</p>
             </div>
             <script>
-            let startDate = document.getElementById('startDate')
+                let startDate = document.getElementById('startDate')
 
-            startDate.addEventListener('change', (e) => {
-                let startDateVal = e.target.value
-                document.getElementById('startDateSelected').innerText = startDateVal
-            })
+                startDate.addEventListener('change', (e) => {
+                    let startDateVal = e.target.value
+                    document.getElementById('startDateSelected').innerText = startDateVal
+                })
             </script>
             <br>
 
@@ -58,14 +57,11 @@
             <div class="mb-3">
                 <label class="form-label">Image</label>
                 <br>
-                <input class="form-control" type="file" id="formFile" name="employee_picture"
-                    onchange="imageUpload(event)" hidden>
+                <input class="form-control" type="file" id="formFile" name="employee_picture" onchange="imageUpload(event)" hidden>
                 <div class="container" style="position:relative; display:inline-block;">
                     <label type="btn" for="formFile" style="position:relative; display:inline-block;">
-                        <img src="/img/add image.png" alt="" id="chg-img" width="100px" height="100px"
-                            style="cursor: pointer; border-radius: 20px; object-fit:cover;">
-                        <i class="fa-solid fa-plus"
-                            style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); font-size:30px">
+                        <img src="/img/add image.png" alt="" id="chg-img" width="100px" height="100px" style="cursor: pointer; border-radius: 20px; object-fit:cover;">
+                        <i class="fa-solid fa-plus" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); font-size:30px">
                         </i>
                         <!-- <a class="" for="formFile"
                             style="position: absolute; bottom: 10%; left: 50%; transform: translate(-50%, -10%); font-size:10px; background-color: #990011; color:white; border-radius:5px; border-color:#990011">Change
@@ -73,31 +69,30 @@
                     </label>
                 </div>
             </div>
+
             <script>
-            const imageUpload = (event) => {
-                const images = event.target.files;
-                const filesLength = images.length;
-                if (filesLength > 0) {
-                    const imageSrc = URL.createObjectURL(images[0]);
-                    const imagePreviewElement = document.querySelector("#chg-img");
-                    imagePreviewElement.src = imageSrc;
-                    imagePreviewElement.style.display = "block";
-                }
-            };
+                const imageUpload = (event) => {
+                    const images = event.target.files;
+                    const filesLength = images.length;
+                    if (filesLength > 0) {
+                        const imageSrc = URL.createObjectURL(images[0]);
+                        const imagePreviewElement = document.querySelector("#chg-img");
+                        imagePreviewElement.src = imageSrc;
+                        imagePreviewElement.style.display = "block";
+                    }
+                };
             </script>
 
             <!-- phone number -->
             <div class="form-group ">
                 <label for="formGroupExampleInput">Phone Number</label>
-                <input type="text" name="employee_phone" class="form-control" id="formGroupExampleInput"
-                    placeholder="Enter new employee phone number*">
+                <input type="text" name="employee_phone" class="form-control" id="formGroupExampleInput" placeholder="Enter new employee phone number*">
             </div>
             <br>
 
             <button type="submit" class="btn" style="background-color: #990011
 ; color: white;">Add Employee</button>
-            <a type="submit" href="/employeeList/<?= $company_id ?>" class="btn btn-outline"
-                style="color:#990011; border:2px solid #990011">Cancel</a>
+            <a type="submit" href="/employeeList/<?= $company_id ?>" class="btn btn-outline" style="color:#990011; border:2px solid #990011">Cancel</a>
         </div>
     </div>
 </form>
